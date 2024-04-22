9dde6ca97733d4a291a10e9bdc1dded6b1fdbe06
TIbor Cus <cus.tibor@outlook.com>
2024-04-22 11:54:48 +0200

    adding accuracy metric

--- assignments/assignment_1/dlvc/metrics.py
+++ assignments/assignment_1/dlvc/metrics.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import torch
+from statistics import mean
 
 class PerformanceMeasure(metaclass=ABCMeta):
     '''
@@ -38,17 +39,20 @@ class Accuracy(PerformanceMeasure):
     Average classification accuracy.
     '''
 
-    def __init__(self, classes) -> None:
+    def __init__(self, classes: tuple[str]) -> None:
         self.classes = classes
-
+        self.classes_counts = {}
+        self.n = 0
+        self.correctly_classified = 0
         self.reset()
 
     def reset(self) -> None:
         '''
         Resets the internal state.
         '''
-        ## TODO implement
-        pass
+        self.n = 0
+        self.n_correct = 0
+        self.classes_counts = { label: {"n_correct":0, "n":0} for label, c in enumerate(self.classes)}
 
     def update(self, prediction: torch.Tensor, 
                target: torch.Tensor) -> None:
@@ -59,16 +63,26 @@ class Accuracy(PerformanceMeasure):
         Raises ValueError if the data shape or values are unsupported.
         '''
 
-        ## TODO implement
-        pass
+        self.n += len(target)
+        prediction = torch.argmax(prediction, 1, keepdim=True)
+        is_correct = prediction == target
+        self.n_correct += sum(is_correct).item()
+        for c in self.classes_counts.keys():
+            indx = target == c
+            self.classes_counts[c]["n_correct"] += sum(is_correct[indx])
+            self.classes_counts[c]["n"] += len(is_correct[indx])
+
+        
 
     def __str__(self):
         '''
         Return a string representation of the performance, accuracy and per class accuracy.
         '''
 
-        ## TODO implement
-        pass
+        accuracy = round(self.accuracy(),4)
+        per_class_accuracy = round(self.per_class_accuracy(),4)
+
+        return f"Accuracy: {accuracy}, Class Accuracy: {per_class_accuracy}"
 
 
     def accuracy(self) -> float:
@@ -76,15 +90,12 @@ class Accuracy(PerformanceMeasure):
         Compute and return the accuracy as a float between 0 and 1.
         Returns 0 if no data is available (after resets).
         '''
-
-        ## TODO implement
-        pass
+        return 0.0 if self.n == 0 else  self.n_correct / self.n
     
     def per_class_accuracy(self) -> float:
         '''
         Compute and return the per class accuracy as a float between 0 and 1.
         Returns 0 if no data is available (after resets).
         '''
-        ## TODO implement
-        pass
+        return mean([ torch.divide(stats["n_correct"], stats["n"]).nan_to_num().item() for stats in self.classes_counts.values()])
        
