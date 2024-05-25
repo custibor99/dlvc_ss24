d7dd53a099a6de6b1cdde88dbd2c2ad03625d267
akaysaban <saban.akay@outlook.com>
2024-05-25 23:23:03 +0200

    fcn on oxford

--- assignments/assignment_2/dlvc/models/resnet50_wrapper.py
+++ assignments/assignment_2/dlvc/models/resnet50_wrapper.py
@@ -5,8 +5,8 @@ from torchvision.models.segmentation import fcn_resnet50
 class Resnet50Wrapper(torch.nn.Module):
     def __init__(self, num_classes = 3, pre_trained=True):
         super(Resnet50Wrapper, self).__init__()
-        weights = "DEFAULT" if pre_trained else None 
-        self.model = fcn_resnet50(weights=weights)
+        weights = "ResNet50_Weights.DEFAULT" if pre_trained else None 
+        self.model = fcn_resnet50(weights_backbone=weights)
         self.model.classifier[4] = torch.nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
 
     def forward(self, x):
--- assignments/assignment_2/train.py
+++ assignments/assignment_2/train.py
@@ -5,40 +5,43 @@ import torch
 import torchvision.transforms.v2 as v2
 from pathlib import Path
 from torchvision.models.segmentation import fcn_resnet50
-
+from torch import optim
 from dlvc.models.segment_model import DeepSegmenter
 from dlvc.dataset.oxfordpets import  OxfordPetsCustom
 from dlvc.metrics import SegMetrics
 from dlvc.trainer import ImgSemSegTrainer
-
-
+from dlvc.utils import LabelDecrementor
+from dlvc.models.resnet50_wrapper import Resnet50Wrapper
 
 def train(args):
 
+    oxford_pets_path = "data/"
     train_transform = v2.Compose([v2.ToImage(), 
                             v2.ToDtype(torch.float32, scale=True),
                             v2.Resize(size=(64,64), interpolation=v2.InterpolationMode.NEAREST),
                             v2.Normalize(mean = [0.485, 0.456,0.406], std = [0.229, 0.224, 0.225])])
     train_transform2 = v2.Compose([v2.ToImage(), 
                             v2.ToDtype(torch.long, scale=False),
-                            v2.Resize(size=(64,64), interpolation=v2.InterpolationMode.NEAREST)])#,
-    
+                            v2.Resize(size=(64,64), interpolation=v2.InterpolationMode.NEAREST),
+                            LabelDecrementor()])#,
+
     val_transform = v2.Compose([v2.ToImage(), 
                             v2.ToDtype(torch.float32, scale=True),
                             v2.Resize(size=(64,64), interpolation=v2.InterpolationMode.NEAREST),
                             v2.Normalize(mean = [0.485, 0.456,0.406], std = [0.229, 0.224, 0.225])])
     val_transform2 = v2.Compose([v2.ToImage(), 
                             v2.ToDtype(torch.long, scale=False),
-                            v2.Resize(size=(64,64), interpolation=v2.InterpolationMode.NEAREST)])
+                            v2.Resize(size=(64,64), interpolation=v2.InterpolationMode.NEAREST),
+                            LabelDecrementor()])
 
-    train_data = OxfordPetsCustom(root="path_to_dataset", 
+    train_data = OxfordPetsCustom(root=oxford_pets_path, 
                             split="trainval",
                             target_types='segmentation', 
                             transform=train_transform,
                             target_transform=train_transform2,
                             download=True)
 
-    val_data = OxfordPetsCustom(root="path_to_dataset", 
+    val_data = OxfordPetsCustom(root=oxford_pets_path, 
                             split="test",
                             target_types='segmentation', 
                             transform=val_transform,
@@ -47,50 +50,67 @@ def train(args):
 
 
 
-    device = ...
+    model = Resnet50Wrapper(3,pre_trained=args.trained)
 
-    model = DeepSegmenter(...)
-    optimizer = ...
-    loss_fn = ...
-    
-    train_metric = SegMetrics(classes=train_data.classes_seg)
-    val_metric = SegMetrics(classes=val_data.classes_seg)
-    val_frequency = 2
 
-    model_save_dir = Path("saved_models")
-    model_save_dir.mkdir(exist_ok=True)
 
-    lr_scheduler = ...
-    
-    trainer = ImgSemSegTrainer(model, 
-                    optimizer,
-                    loss_fn,
-                    lr_scheduler,
-                    train_metric,
-                    val_metric,
-                    train_data,
-                    val_data,
-                    device,
-                    args.num_epochs, 
-                    model_save_dir,
-                    batch_size=64,
-                    val_frequency = val_frequency)
+    optimizer = optim.Adam(model.parameters())
+    lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma = 0.09)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    device = torch.device("cpu")
+
+    segMetricsTrain = SegMetrics(train_data.classes_seg)
+    segMetricsVal = SegMetrics(val_data.classes_seg)
+
+    path = f"weights/fcn_resnet50_weighted_{args.trained}.pt"
+
+    trainer = ImgSemSegTrainer(
+        model,
+        optimizer,
+        loss_fn,
+        lr_scheduler,
+        segMetricsTrain,
+        segMetricsVal,
+        train_data,
+        val_data,
+        device,
+        5,
+        path,
+        batch_size=128,
+        val_frequency=1
+    )
+
+    print("Training, weighted: ", args.trained)
+
     trainer.train()
 
-    # see Reference implementation of ImgSemSegTrainer
-    # just comment if not used
-    trainer.dispose() 
+    print("\nTraining done.\n")
+    train_metrics = trainer.metrics_train
+    val_metrics = trainer.metrics_val
+
+    # datafame is a list of tuples (loss, mIoU)
+    train_loss = [x[0] for x in train_metrics]
+    train_mIoU = [x[1] for x in train_metrics]
+    val_loss = [x[0] for x in val_metrics]
+    val_mIoU = [x[1] for x in val_metrics]
+
+    print(f"Train mIoU: {train_mIoU}")
+    print(f"Val mIoU: {val_mIoU}")
+
+    print(f"Train loss: {train_loss}")
+    print(f"Val loss: {val_loss}")
+
+    name = f"fcn_resnet50_weighted_{args.trained}_metrics.csv"
+    # save as epoch, loss, mIoU
+    with open(name, "w") as f:
+        f.write("epoch,loss,mIoU\n")
+        for i in range(len(train_loss)):
+            f.write(f"{i},{train_loss[i]},{train_mIoU[i]}\n")
+
+    f.close()
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser(description='Training')
-    args.add_argument('-d', '--gpu_id', default='0', type=str,
-                      help='index of which GPU to use')
-    
-    if not isinstance(args, tuple):
-        args = args.parse_args()
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_id)
-    args.gpu_id = 0
-    args.num_epochs = 31
-
+    args.add_argument('--trained', type=str, default='True', help='Use pretrained weights')
 
     train(args)
